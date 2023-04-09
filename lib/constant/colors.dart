3a34e25b0b10af614be29b6228b4c9be1c942346
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-04-09 18:28:50 +0700

    asset: colors, styles, minimize main

---
+++ lib/constant/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBackgroundColor = Color(0xFFF9F9F9);
+  static const LinearGradient primaryGradient =
+      LinearGradient(colors: [Color(0xFF6FE3E1), Color(0xFF5257E5)]);
+  static const Color secondaryColor = Color(0xFFFFFFFF);
+  static const Color primaryTextColor = Color(0xFF323945);
+  static Color primaryHintTextColor = const Color(0xFFCCCCCC).withOpacity(0.6);
+  static const Color authenticationSmallTextColor = Color(0xFF6FE3E1);
+  static Color primaryShadowColor = const Color(0xFF5A6CEA).withOpacity(0.07);
+  static Color secondaryShadowColor = const Color(0xFF000000).withOpacity(0.25);
+}
--- lib/constant/styles.dart
+++ lib/constant/styles.dart
@@ -1,1 +1,15 @@
-class AppStyle {}
+import 'package:bookvies/constant/colors.dart';
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static Shadow primaryShadow = Shadow(
+    color: AppColors.primaryShadowColor,
+    offset: const Offset(12, 26),
+    blurRadius: 40,
+  );
+
+  static Shadow secondaryShadow = Shadow(
+      color: AppColors.secondaryShadowColor,
+      offset: const Offset(0, 10),
+      blurRadius: 30);
+}
