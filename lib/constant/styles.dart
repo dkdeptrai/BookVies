baafd36d99b744e7fb7341980e92375f81c6b0b0
khoadz <khoa.soft.e@gmail.com>
2023-04-09 20:26:23 +0700

    feat: working on signup screen

--- lib/constant/styles.dart
+++ lib/constant/styles.dart
@@ -1,1 +1,9 @@
-class AppStyle {}
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static const TextStyle authenticationHeader = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    fontFamily: "Poppins",
+  );
+}
