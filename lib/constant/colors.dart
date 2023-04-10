7866d1cb28416b0349fc22156056bb009a4832bc
khoadz <khoa.soft.e@gmail.com>
2023-04-10 21:19:51 +0700

    feat: modifying colors

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -6,6 +6,7 @@ class AppColors {
       LinearGradient(colors: [Color(0xFF6FE3E1), Color(0xFF5257E5)]);
   static const Color secondaryColor = Color(0xFFFFFFFF);
   static const Color primaryTextColor = Color(0xFF323945);
+  static const Color greyTextColor = Color(0xFFCCCCCC);
   static Color primaryHintTextColor = const Color(0xFFCCCCCC).withOpacity(0.6);
   static const Color authenticationSmallTextColor = Color(0xFF6FE3E1);
   static Color primaryShadowColor = const Color(0xFF5A6CEA).withOpacity(0.07);
