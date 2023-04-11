06134e087ba5dc6eaaf909f3035666db37e65e44
khoadz <khoa.soft.e@gmail.com>
2023-04-11 13:44:45 +0700

    feat: added routes

---
+++ lib/utils/router.dart
@@ -0,0 +1,14 @@
+import 'package:bookvies/screens/sign_up_screen/sign_up_screen.dart';
+import 'package:bookvies/screens/login_screen/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      //   case (SignUpScreen.id):
+      //     return MaterialPageRoute(builder: (_) => const SignUpScreen());
+      //   case (LoginScreen.id):
+      //     return MaterialPageRoute(builder: (_) => const LoginScreen());
+    }
+  }
+}
